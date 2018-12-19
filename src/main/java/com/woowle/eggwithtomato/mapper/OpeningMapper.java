3e1d97086440bb858854f7bfd87763b7c55c4257
xiaoyin.lu.o <xiaoyin.lu.o@nio.com>
2018-12-19 16:53:05 +0800

    init

---
+++ src/main/java/com/woowle/eggwithtomato/mapper/OpeningMapper.java
@@ -0,0 +1,10 @@
+package com.woowle.eggwithtomato.mapper;
+
+import com.woowle.eggwithtomato.model.Opening;
+
+/**
+ * Create By xiaoyin.lu.o on 2018/12/19
+ **/
+public interface OpeningMapper extends Mapper<Opening> {
+
+}
